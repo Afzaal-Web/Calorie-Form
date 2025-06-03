f1ae005332ea015d56646ee2c3edcf9613130175
Afzaal-Web <afzaalwebsolutions@gmail.com>
2025-06-03 21:06:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
-const output = document.getElementById('output');
-
+// const output = document.getElementById('output');
+const calorieResult = document.getElementById('calorie-result');
+const calorieCloseBtn = document.getElementById('calorie-close');
+const overlay = document.querySelector('.overlay');
+const displayBudgetCalories = document.getElementById('budgeted-calories');
+const displayConsumedCalories = document.getElementById('consumed-calories');
+const displayBurnedCalories = document.getElementById('burned-calories');
+const calorieStatus = document.getElementById('calorie-status');
 let isError = false;
-
 
 /*NOTE: Values from an HTML input field are received as strings in JavaScript.
  You'll need to convert these strings into numbers before performing any calculations.
@@ -30,59 +35,122 @@ function isInvalidInput(str) {
 // the above will print
 // [ '1e3', index: 0, input: '1e3', groups: undefined ]
 
-function addEntry() {
-  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
-  const HTMLString = `
-  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
-   <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-  <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />
+const entryCounters = {
+  breakfast: 0,
+  lunch: 0,
+  dinner: 0,
+  snacks: 0,
+  exercise: 0
+};
 
+function addEntry() {
+  const currentCategory = entryDropdown.value;
+  const targetInputContainer = document.querySelector(`#${currentCategory} .input-container`);
+  // const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length +1;
+
+  entryCounters[currentCategory] = entryCounters[currentCategory] + 1;
+  const entryNumber = entryCounters[currentCategory];
+  const HTMLString = `
+    <div class="entry-row">
+    <label for="${currentCategory}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" id="${currentCategory}-${entryNumber}-name" placeholder="Name" />
+    <label for="${currentCategory}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" id="${currentCategory}-${entryNumber}-calories" placeholder="Calories" />
+    <button type="button" id="${currentCategory}-button-${entryNumber}" class="delete-entry">❌</button>
+  </div>
   `;
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+  // deleteEntryfun(currentCategory,entryNumber);
 }
 
-addEntryButton.addEventListener('click', addEntry);
+// add evenet listener on button unique id
 
+/* function deleteEntryfun(currentCategory,entryNumber) {
+  const deleteButton = document.getElementById(`${currentCategory}-button-${entryNumber}`);
+  if (deleteButton) {
+    deleteButton.addEventListener('click', function eventHandler(e) {
+      console.log(e);
+      const entryRow = e.target.closest('.entry-row');
+      if (entryRow) {
+        const confirmed = confirm("Delete this entry? It will be not restored");
+        if (confirmed) {
+          entryRow.remove();
+          deleteButton.removeEventListener('click', eventHandler);
+          console.log('Event is removed now');
+        }
+      }
+    });
+}
+} */
+
+
+// event delegation 
+const inputContainers = document.querySelectorAll('.input-container');
+
+inputContainers.forEach(container => {
+  container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete-entry')) {
+      const entryRow = e.target.closest('.entry-row');
+      if (entryRow) {
+        showConfirm("Delete this entry? It will not be restored.", function (confirm) {
+          if (confirm) {
+            entryRow.remove();
+          } 
+
+        });
+      }
+    }
+  });
+});
+
+addEntryButton.addEventListener('click', addEntry);
 
 function calculateCalories(e) {
   e.preventDefault();
   isError = false;
   const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
-  console.log(breakfastNumberInputs);
   const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
   const dinnerNumberInputs = document.querySelectorAll("#dinner input[type='number']");
   const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
   const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
-
   const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
   const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
-
   const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
-
   if (isError) {
     return
   }
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
-
   const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
 
-  output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
-  <hr>
-   <p>${budgetCalories} Calories Budgeted</p>
-   <p>${consumedCalories} Calories Consumed</p>
-    <p>${exerciseCalories} Calories Burned</p>
-  `;
-  output.classList.remove('hide');
+  // output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+  // <hr>
+  //  <p>${budgetCalories} Calories Budgeted</p>
+  //  <p>${consumedCalories} Calories Consumed</p>
+  //   <p>${exerciseCalories} Calories Burned</p>
+  // `;
+  // output.classList.remove('hide');
 
+  calorieResult.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  displayBudgetCalories.textContent = budgetCalories;
+  displayConsumedCalories.textContent = consumedCalories;
+  displayBurnedCalories.textContent = exerciseCalories;
+  calorieStatus.classList.add(surplusOrDeficit.toLowerCase());
+  calorieStatus.textContent = `${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}`;
 }
-
 calorieCounter.addEventListener('submit', calculateCalories);
+
+function hideCalorieModal() {
+  calorieResult.classList.add('hidden');
+  overlay.classList.add('hidden');
+  calorieStatus.textContent = "";
+  calorieStatus.classList.remove("surplus", "deficit");
+}
+calorieCloseBtn.addEventListener('click', hideCalorieModal);
 
 
 
@@ -100,18 +168,40 @@ function getCaloriesFromInputs(list) {
       return null;
     }
     calories += Number(currVal);
-
   }
   return calories;
 }
 
-function clearForm (){
+function clearForm() {
   const inputContainers = Array.from(document.querySelectorAll('.input-container'));
-  for(const container of inputContainers){
+  for (const container of inputContainers) {
     container.innerHTML = "";
   }
-budgetNumberInput.value = "";
-output.innerText = "";
-output.classList.add('hide');
+  budgetNumberInput.value = "";
+  output.innerText = "";
+  output.classList.add('hide');
 }
 clearButton.addEventListener('click', clearForm);
+
+let confirmCallback = null;
+function showConfirm(message, callback) {
+  document.getElementById("confirm-message").textContent = message;
+  document.getElementById("custom-confirm").classList.remove("hidden");
+  confirmCallback = callback;
+}
+
+document.getElementById("confirm-yes").addEventListener("click", function () {
+  document.getElementById("custom-confirm").classList.add("hidden");
+  if (typeof confirmCallback === "function") {
+    confirmCallback(true);
+  }
+});
+
+document.getElementById("confirm-no").addEventListener("click", function () {
+  document.getElementById("custom-confirm").classList.add("hidden");
+  if (typeof confirmCallback === "function") {
+    confirmCallback(false);
+  }
+
+});
+
